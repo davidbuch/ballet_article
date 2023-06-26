4f56bf9a52d14ee9c627c9ef7b55fc19a7730c33
David Buch <davidbuch42@gmail.com>
2023-06-25 21:46:38 -0400

    Removing unused scripts

--- scripts/sky_survey_analysis/edsgc_tree_dirichlet.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-galaxy_data <- read.csv("data/intermediate_data/primary/edsgc.csv")
-galaxy_clusters <- read.csv("data/intermediate_data/primary/edcci_clusters.csv")
-
-probs <- fit_tree_dirichlet(galaxy_data)
-saveRDS(probs, file = "fitted_models/primary/edsgc_tree_dirichlet.rds")
